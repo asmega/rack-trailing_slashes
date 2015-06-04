ca99d507b316042d7860172ef8eb9b1016aeb90a
Phil Lee <asmega@ph-lee.com>
2015-06-04 15:47:36 +0100

    init gem

---
+++ Gemfile
@@ -0,0 +1,4 @@
+source 'https://rubygems.org'
+
+# Specify your gem's dependencies in rack-trailing_slashes.gemspec
+gemspec
---
+++ lib/rack/trailing_slashes.rb
@@ -0,0 +1,7 @@
+require "rack/trailing_slashes/version"
+
+module Rack
+  module TrailingSlashes
+    # Your code goes here...
+  end
+end
